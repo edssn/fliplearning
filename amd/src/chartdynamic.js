1e90102e869741639a1f86692774a9391efebf0d
Edisson Fernando Sigua Loja <edissonf.sigua@gmail.com>
2021-03-10 02:07:02 -0500

    Agregado boton de exportar a graficos

--- amd/src/chartdynamic.js
+++ amd/src/chartdynamic.js
@@ -3,8 +3,8 @@ define([
         'highcharts/highcharts-3d',
         'highcharts/highcharts-more',
         'highcharts/modules/heatmap',
-        // 'highcharts/modules/exporting',
-        // 'highcharts/modules/export-data',
+        'highcharts/modules/exporting',
+        'highcharts/modules/export-data',
         'highcharts/modules/accessibility',
         'highcharts/modules/no-data-to-display',
         ],
@@ -16,7 +16,57 @@ define([
             return { }
         },
         mounted() {
-            (this.lang) && Highcharts.setOptions({lang: this.lang, credits: { enabled: false }});
+            (this.lang) && Highcharts.setOptions({
+                lang: this.lang,
+                credits: { enabled: false },
+                exporting: {
+                    buttons: {
+                        contextButton: {
+                            menuItems: [{
+                                text: this.lang.downloadPNG,
+                                onclick: function () {
+                                    this.exportChart({
+                                        type: 'image/png'
+                                    });
+                                }
+                            },{
+                                text: this.lang.downloadJPEG,
+                                onclick: function () {
+                                    this.exportChart({
+                                        type: 'image/jpeg'
+                                    });
+                                }
+                            },{
+                                text: this.lang.downloadPDF,
+                                onclick: function () {
+                                    this.exportChart({
+                                        type: 'application/pdf'
+                                    });
+                                }
+                            },{
+                                text: this.lang.downloadSVG,
+                                onclick: function () {
+                                    this.exportChart({
+                                        type: 'image/svg+xml'
+                                    });
+                                }
+                            },{
+                                text: this.lang.downloadXLS,
+                                onclick: function () {
+                                    this.downloadXLS();
+                                }
+                            },{
+                                text: this.lang.downloadCSV,
+                                onclick: function () {
+                                    this.downloadCSV();
+                                }
+                            }],
+                            symbol: 'menuball',
+                            symbolStroke: '#118AB2'
+                        }
+                    }
+                }
+            });
             this._highchart = Highcharts.chart(this.container, this.chart);
         },
         watch: {
